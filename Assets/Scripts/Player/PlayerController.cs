5037123d8a3867868db3029a8edab0fb1a4e5095
DWhittley <115023269+DWhittley@users.noreply.github.com>
2022-11-01 16:21:06 -0400

    Update PlayerController.cs
    adjusted jump height reduction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -57,7 +57,7 @@ public class PlayerController : MonoBehaviour
         {
             StopCoroutine(jumpForceChange);
             jumpForceChange = null;
-            jumpForce /= 2;
+            jumpForce /= 1.5f;
             jumpForceChange = StartCoroutine(JumpForceChange());
 
         }
